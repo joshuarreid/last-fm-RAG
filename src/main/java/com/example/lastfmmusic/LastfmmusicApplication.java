ebf6b2231577cfe5afde1ab57d5eb4e7a8553b25
joshuarreid <2001josh@gmail.com>
2025-08-19 17:29:34 -0400

    Initial commit: last.fm RAG music bot

---
+++ src/main/java/com/example/lastfmmusic/LastfmmusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.lastfmmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastfmmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastfmmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lastfmmusic/LastfmmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lastfmmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastfmmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
